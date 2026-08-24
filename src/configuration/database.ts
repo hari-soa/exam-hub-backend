27957b1bef3d4b77e2a8f5d22ec481dcbb8df764
hari-soa <hei.zarasoa@gmail.com>
2026-08-24 14:22:54 +0300

    feat: export pool

--- src/configuration/database.ts
+++ src/configuration/database.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = new Pool({
+export const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
