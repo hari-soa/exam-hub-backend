422e05c9a5db9cff3c3b4bb93ea321c60e6e4bf6
hari-soa <hei.zarasoa@gmail.com>
2026-08-24 16:12:24 +0300

    feat: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
-import { errorHandler } from "./middlewares/errorMiddleware";
+import adminRoutes from "./routes/adminRoutes";
+import studentRoutes from "./routes/studentRoutes";
+import { errorHandler } from "./security/errorMiddleware";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const clientUrl = process.env.CLIENT_URL || "http://localhost:5173";
 
 app.use(cors({ origin: clientUrl, credentials: true }));
@@ -15,6 +17,9 @@ app.get("/", (req, res) => {
 });
 
 app.use(authRoutes);
+app.use(adminRoutes);
+app.use(studentRoutes);
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
